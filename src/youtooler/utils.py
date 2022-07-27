6d195f20ac277700a7d9b98b03f008f1a4bfd056
Fraccs <aliprandifrancescopp@gmail.com>
2022-07-27 13:49:24 +0200

    refactor(app): move print_logo to utils

--- src/youtooler/utils.py
+++ src/youtooler/utils.py
@@ -3,8 +3,19 @@ import requests
 import string_utils
 from argparse import ArgumentParser
 from bs4 import BeautifulSoup
+from colorama import Fore, Back, Style
 from sys import stderr
 from .exceptions import *
+
+def print_logo():
+    print(f'{Style.BRIGHT}')
+    print(f'{Fore.MAGENTA}                         _____.___.           {Fore.CYAN}___________           .__                ')
+    print(f'{Fore.MAGENTA}               />        \\__  |   | ____  __ _{Fore.CYAN}\\__    ___/___   ____ |  |   {Fore.MAGENTA}___________ ')
+    print(f'{Fore.MAGENTA}  ()          //----------/   |   |/  _ \\|  |  \\{Fore.CYAN}|    | /  _ \\ /  _ \\|  | {Fore.MAGENTA}_/ __ \\_  __ \\----------\\')
+    print(f'{Fore.YELLOW} (*)OXOXOXOXO(*>          \\____   (  <_> )  |  /{Fore.CYAN}|    |(  <_> |  <_> )  |_{Fore.YELLOW}\\  ___/|  | \\/           \\')
+    print(f'{Fore.YELLOW}  ()          \\\\----------/ ______|\\____/|____/ {Fore.CYAN}|____| \\____/ \\____/|____/{Fore.YELLOW}\\___  >__|---------------\\   ')
+    print(f'{Fore.YELLOW}               \\>         \\/                                      {Fore.YELLOW}            \\/       ')
+    print(f'\n{Fore.WHITE}{Back.RED}Developers assume no liability and are not responsible for any misuse or damage caused by this program.{Style.RESET_ALL}')
 
 def get_arguments():
     '''
