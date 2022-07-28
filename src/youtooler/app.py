fd28d5597a73cde73c775fae6fa65b1f5e3b0c24
Fraccs <aliprandifrancescopp@gmail.com>
2022-07-28 20:03:09 +0200

    fix(app): add __clean__ handler on SIGINT, SIGTERM
    The __clean__ handler is now run also when SIGINT or SIGTERM are caught
    by the program

--- src/youtooler/app.py
+++ src/youtooler/app.py
@@ -1,24 +1,31 @@
 import atexit
 import shutil
 import os
+import signal
 from sys import stderr
 from .logs import get_error_message, get_log_message
 from .thread import YoutoolerThread
 from .utils import get_video_duration
 
-SOCKS_PORT = 9150
-CONTROL_PORT = 9151
-
 class Youtooler:
+    SOCKS_PORT = 9150
+    CONTROL_PORT = 9151
+    PATH = '/tmp/youtooler'
+
     def __init__(self, url: str, level: int):
-        self.__storage_directory_path = self.__create_storage_dir__()
+        # Register cleanup handlers
+        signal.signal(signal.SIGINT, self.__clean__)
+        signal.signal(signal.SIGTERM, self.__clean__)
+        atexit.register(self.__clean__)
+
         self.threads = []
         self.level = level
         self.url = url
-        self.socks_ports = [port for port in range(SOCKS_PORT, SOCKS_PORT + (level * 2), 2)]
-        self.control_ports = [port for port in range(CONTROL_PORT, CONTROL_PORT + (level * 2), 2)]
+        self.socks_ports = [port for port in range(self.SOCKS_PORT, self.SOCKS_PORT + (level * 2), 2)]
+        self.control_ports = [port for port in range(self.CONTROL_PORT, self.CONTROL_PORT + (level * 2), 2)]
+        self.__create_storage_dir__()
 
-    def start(self):
+    def start(self) -> None:
         video_duration = get_video_duration(self.url)
 
         for port in self.socks_ports:
@@ -27,44 +34,32 @@ class Youtooler:
         for thread in self.threads:
             thread.setDaemon(True)
             thread.start()
-        
-        atexit.register(self.stop)
 
-    def stop(self):
+    def __clean__(self, *args) -> None:
         '''
-        Stops the execution of the threads and their subprocesses
+        Removes the temporary storage directory and its subdirectories
         '''
+
         try:
             for thread in self.threads:
                 thread.join()
         except AttributeError:
             pass
 
-        self.__clean__()
-
-    def __clean__(self):
-        '''
-        Removes the temporary storage directory and its subdirectories
-        '''
-
         try:
-            shutil.rmtree(self.__storage_directory_path)
+            shutil.rmtree(self.PATH)
         except OSError:
             pass
 
-    def __create_storage_dir__(self) -> str:
+    def __create_storage_dir__(self) -> None:
         '''
         Creates the temporary storage directory of the program ('/tmp/youtooler') and returns its path
         '''
 
-        PATH = '/tmp/youtooler'
-
         try:
-            os.mkdir(PATH)
+            os.mkdir(self.PATH)
         except FileExistsError:
             print(get_error_message('STORAGE-NOT-CREATED'), file=stderr)
             exit()
         else:
             print(get_log_message('STORAGE-CREATED'))
-
-        return PATH
